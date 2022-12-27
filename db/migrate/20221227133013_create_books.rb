e4df2ebb8fdf88a00eb5c0373c7cd799e249f628
AsheDuran <bady.0222.monk@gmail.com>
2022-12-27 13:37:45 +0000

    新規作成

--- db/migrate/20221227133013_create_books.rb
+++ db/migrate/20221227133013_create_books.rb
@@ -1,7 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.text :body
+      t.integer :user_id
       t.timestamps
     end
   end

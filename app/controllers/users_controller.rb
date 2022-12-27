7adaaed0e10c97f7b2908607ad7513e0d4bb4712
AsheDuran <bady.0222.monk@gmail.com>
2022-12-27 14:14:09 +0000

    users作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   root :to =>"homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

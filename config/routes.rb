17cef0148b46665a41cd94c6605ac9435eab2752
AsheDuran <bady.0222.monk@gmail.com>
2022-12-28 14:45:57 +0000

    users/routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/index'
-  get 'users/show'
+
   devise_for :users
   root :to =>"homes#top"
+  resources :users, only: [:index,:show,:edit,:update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

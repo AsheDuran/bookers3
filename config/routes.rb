48cc4020e9d999ec4abb6d001679967c145fdf19
AsheDuran <bady.0222.monk@gmail.com>
2022-12-28 15:08:21 +0000

    books_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:new,:index,:show,:edit,:update,:delete]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

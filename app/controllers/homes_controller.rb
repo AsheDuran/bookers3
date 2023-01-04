f9f9efef980af4258439575ecfadc2b6b628e398
AsheDuran <brady.0222.monk@gmail.com>
2023-01-04 13:41:03 +0000

    aboutページ追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,4 +3,7 @@ class HomesController < ApplicationController
   def top
   end
 
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root :to =>"homes#top"
+  get "home/about"=>"homes#about"
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:new,:index,:show,:edit,:update,:delete]
 

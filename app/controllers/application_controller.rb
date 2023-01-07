606b9a3c123d522d72a12cf3c3c97f975d058b64
AsheDuran <brady.0222.monk@gmail.com>
2023-01-07 13:48:14 +0000

    application_controllerの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end

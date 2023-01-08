84097e39f6d0034c0380253e69f908914b2a00e9
AsheDuran <brady.0222.monk@gmail.com>
2023-01-08 14:59:06 +0000

    ActiveStorage追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_profile_image.png')
+      profile_image.attach(io: File.open(file_path), filename: 'no_profile_image.png', content_type: 'image/png')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end

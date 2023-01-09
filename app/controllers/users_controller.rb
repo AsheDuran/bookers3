789b19361e0c3178c7ff142ba4d7fed50a54f692
AsheDuran <brady.0222.monk@gmail.com>
2023-01-09 12:23:25 +0000

    users_controller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
 end

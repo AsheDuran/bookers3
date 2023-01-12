e05d16458510f4eefa72d952e5102fe771530532
AsheDuran <brady.0222.monk@gmail.com>
2023-01-12 12:53:50 +0000

    books_controller修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,17 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   resources :users, only: [:index,:show,:edit,:update]
-  resources :books, only: [:new,:index,:show,:edit,:update,:delete]
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

0764529896d0dfe0f02b4a0d20ed066b1796ce22
AsheDuran <brady.0222.monk@gmail.com>
2023-01-13 14:25:32 +0000

    books_index修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    # logger.debug("投稿成功")
+    logger.debug("投稿成功")
     redirect_to books_path
   end
 

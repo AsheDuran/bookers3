cfea303b6c885786ef8296bf019c4b85ab7df96c
AsheDuran <brady.0222.monk@gmail.com>
2023-01-16 11:51:48 +0000

    log_in機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,13 +16,7 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
- 
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-    end
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
